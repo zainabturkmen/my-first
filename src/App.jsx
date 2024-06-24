a1661d1c769026a26b8f4f664ecb80d9341eca64
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-24 17:17:20 +0430

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import Navbar from './Componenets/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import styled from 'styled-components'
 
 function App() {
 
