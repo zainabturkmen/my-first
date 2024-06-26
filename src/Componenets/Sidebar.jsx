6f97045214b33fb721159d4e9c95def8423c9d14
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-26 12:04:59 +0430

    updating sidebar

---
+++ src/Componenets/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useState } from 'react';
+
+const Sidebar = () => {
+    const [isOpen, setIsOpen] = useState(true);
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Sidebar;
