53313f6ac5b5f899cc2caafb6df36d182f4eb128
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-28 15:21:38 +0430

    updating sidebar

--- src/Componenets/Navbar.jsx
+++ src/Componenets/Navbar.jsx
@@ -8,10 +8,17 @@ import Sidebar from "./Sidebar";
 const Navbar = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+  // const toggleSidebar = () => {
+  //   setIsSidebarOpen(!isSidebarOpen);
+  // };
+
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
+    setTimeout(() => {
+      setIsSidebarOpen(!isSidebarOpen);
+    }, 300); // Delay of 300ms (adjust as needed)
   };
-
+  
   return (
     <Wrapper>
       <div className="navbar">
@@ -65,7 +72,7 @@ const Wrapper = styled.header`
 
   nav {
     display: none;
-    /* transition: all 0.3s ease-in-out; */
+    transition: all 0.3s ease-in-out;
   }
 
   .logo-and-toggle-div {
