a7da3ba1b5e3d13d01fe4b6e86e922f4b85c21c4
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-29 08:30:43 +0430

    updating sidebar

--- src/Componenets/Sidebar.jsx
+++ src/Componenets/Sidebar.jsx
@@ -5,22 +5,11 @@ import logo from "../assets/logo.png";
 import styled from "styled-components";
 
 const Sidebar = ({ isSidebarOpen, toggleSidebar }) => {
-  useEffect(() => {
-    const sidebar = document.querySelector(".sidebar");
-    if (isSidebarOpen) {
-      sidebar.classList.add("show-sidebar");
-      sidebar.style.transform = "translateX(0)";
-    } else {
-      sidebar.classList.remove("show-sidebar");
-      sidebar.style.transform = "translateX(-100%)";
-    }
-  }, [isSidebarOpen]);
 
   return (
     <SidebarContainer>
       <aside
-        className="sidebar"
-        // className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}
+        className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}
       >
         <div className="sidebar-header">
           <img src={logo} className="logo" alt="zainab logo" />
@@ -108,7 +97,7 @@ const SidebarContainer = styled.div`
     height: 100%;
     background: white;
     transition: all 0.3s linear;
-    transform: translate(-100%);
+    transform: translate(100%);
     box-shadow: var(--bs);
   }
   .show-sidebar {
